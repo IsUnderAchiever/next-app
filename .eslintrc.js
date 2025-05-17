7948a4e868fb6d079b517e5d891901a5901e9d70
tong <938798576@qq.com>
2025-05-17 11:23:34 +0800

    refactor: 精简 Prettier 和 ESLint 配置文件
    移除重复的配置项，将 Prettier 配置从 ESLint 配置中分离，以提高配置文件的清晰度和可维护性

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,84 @@
 module.exports = {
-  root: true, // 确保 ESLint 不会向上查找配置文件
-  parser: '@typescript-eslint/parser', // 使用 TypeScript 解析器
+  // 确保 ESLint 不会向上查找配置文件
+  root: true,
+  // 使用 TypeScript 解析器
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020, // 支持 ES2020 语法
-    sourceType: 'module', // 支持 ES 模块
+    // 支持 ES2020 语法
+    ecmaVersion: 2020,
+    // 支持 ES 模块
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true, // 支持 JSX
+      // 支持 JSX
+      jsx: true,
     },
   },
   env: {
-    browser: true, // 浏览器环境
-    node: true, // Node.js 环境
-    es6: true, // ES6+ 语法支持
+    // 浏览器环境
+    browser: true,
+    // Node.js 环境
+    node: true,
+    // ES6+ 语法支持
+    es6: true,
   },
   extends: [
-    'eslint:recommended', // ESLint 推荐规则
-    'plugin:@typescript-eslint/recommended', // TypeScript 推荐规则
-    'plugin:react/recommended', // React 推荐规则
-    'plugin:react/jsx-runtime', // 支持 React 17+ 的 JSX 运行时
-    'plugin:prettier/recommended', // Prettier 推荐规则
-    'prettier', // 禁用与 Prettier 冲突的 ESLint 规则
+    // ESLint 推荐规则
+    'eslint:recommended',
+    // TypeScript 推荐规则
+    'plugin:@typescript-eslint/recommended',
+    // React 推荐规则
+    'plugin:react/recommended',
+    // 支持 React 17+ 的 JSX 运行时
+    'plugin:react/jsx-runtime',
+    // Prettier 推荐规则
+    'plugin:prettier/recommended',
+    // 禁用与 Prettier 冲突的 ESLint 规则
+    'prettier',
   ],
   plugins: [
-    'react', // React 插件
-    'prettier', // Prettier 插件
-    '@typescript-eslint', // TypeScript ESLint 插件
+    // React 插件
+    'react',
+    // Prettier 插件
+    'prettier',
+    // TypeScript ESLint 插件
+    '@typescript-eslint',
   ],
   rules: {
-    // ESLint 规则
-    'no-unused-vars': 'warn', // 允许未使用的变量，但发出警告
-    'no-console': 'warn', // 允许 console，但发出警告
-    'react/react-in-jsx-scope': 'off', // React 17+ 不需要此规则
-    'react/jsx-uses-react': 'off', // React 17+ 不需要此规则
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }], // 允许在 JSX 文件中使用 TS/JS
+    // 允许未使用的变量，但发出警告
+    'no-unused-vars': 'warn',
+    // 允许 console，但发出警告
+    'no-console': 'warn',
+    // React 17+ 不需要此规则
+    'react/react-in-jsx-scope': 'off',
+    // React 17+ 不需要此规则
+    'react/jsx-uses-react': 'off',
+    // 允许在 JSX 文件中使用 TS/JS
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
 
-    // Prettier 规则
     'prettier/prettier': [
       'error',
       {
-        singleQuote: true, // 使用单引号
-        semi: true, // 使用分号
-        trailingComma: 'all', // 在对象或数组的最后一个元素后添加逗号
-        printWidth: 80, // 每行的最大字符数
-        tabWidth: 2, // 缩进宽度
-        useTabs: false, // 不使用制表符缩进
-        endOfLine: 'auto', // 自动处理换行符
+        // 使用单引号
+        singleQuote: true,
+        // 使用分号
+        semi: true,
+        // 在对象或数组的最后一个元素后添加逗号
+        trailingComma: 'all',
+        // 每行的最大字符数
+        printWidth: 80,
+        // 缩进宽度
+        tabWidth: 2,
+        // 不使用制表符缩进
+        useTabs: false,
+        // 自动处理换行符
+        endOfLine: 'auto',
       },
     ],
   },
   settings: {
     react: {
-      version: 'detect', // 自动检测 React 版本
+      // 自动检测 React 版本
+      version: 'detect',
     },
   },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,84 +1,22 @@
 module.exports = {
-  // 确保 ESLint 不会向上查找配置文件
-  root: true,
-  // 使用 TypeScript 解析器
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    // 支持 ES2020 语法
-    ecmaVersion: 2020,
-    // 支持 ES 模块
-    sourceType: 'module',
-    ecmaFeatures: {
-      // 支持 JSX
-      jsx: true,
-    },
-  },
-  env: {
-    // 浏览器环境
-    browser: true,
-    // Node.js 环境
-    node: true,
-    // ES6+ 语法支持
-    es6: true,
-  },
-  extends: [
-    // ESLint 推荐规则
-    'eslint:recommended',
-    // TypeScript 推荐规则
-    'plugin:@typescript-eslint/recommended',
-    // React 推荐规则
-    'plugin:react/recommended',
-    // 支持 React 17+ 的 JSX 运行时
-    'plugin:react/jsx-runtime',
-    // Prettier 推荐规则
-    'plugin:prettier/recommended',
-    // 禁用与 Prettier 冲突的 ESLint 规则
-    'prettier',
-  ],
-  plugins: [
-    // React 插件
-    'react',
-    // Prettier 插件
-    'prettier',
-    // TypeScript ESLint 插件
-    '@typescript-eslint',
-  ],
-  rules: {
-    // 允许未使用的变量，但发出警告
-    'no-unused-vars': 'warn',
-    // 允许 console，但发出警告
-    'no-console': 'warn',
-    // React 17+ 不需要此规则
-    'react/react-in-jsx-scope': 'off',
-    // React 17+ 不需要此规则
-    'react/jsx-uses-react': 'off',
-    // 允许在 JSX 文件中使用 TS/JS
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
-
-    'prettier/prettier': [
-      'error',
-      {
-        // 使用单引号
-        singleQuote: true,
-        // 使用分号
-        semi: true,
-        // 在对象或数组的最后一个元素后添加逗号
-        trailingComma: 'all',
-        // 每行的最大字符数
-        printWidth: 80,
-        // 缩进宽度
-        tabWidth: 2,
-        // 不使用制表符缩进
-        useTabs: false,
-        // 自动处理换行符
-        endOfLine: 'auto',
-      },
-    ],
-  },
-  settings: {
-    react: {
-      // 自动检测 React 版本
-      version: 'detect',
-    },
-  },
+  // 使用单引号
+  singleQuote: true,
+  // 使用分号
+  semi: true,
+  // 在对象或数组的最后一个元素后添加逗号
+  trailingComma: 'all',
+  // 每行的最大字符数
+  printWidth: 80,
+  // 缩进宽度
+  tabWidth: 2,
+  // 不使用制表符缩进
+  useTabs: false,
+  // JSX 中使用单引号
+  jsxSingleQuote: true,
+  // 对象字面量的括号是否换行
+  bracketSpacing: true,
+  // JSX 的闭合括号是否在同一行
+  jsxBracketSameLine: false,
+  // 自动处理换行符
+  endOfLine: 'auto'
 }
